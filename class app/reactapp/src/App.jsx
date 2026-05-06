db6fa0faaea0b23d5733228c402d0ab8e597304f
Kaushal <ykaushal938@gmail.com>
2026-05-06 05:54:45 +0530

    feat added debouncing code

--- class app/reactapp/src/App.jsx
+++ class app/reactapp/src/App.jsx
@@ -8,19 +8,31 @@ import Sample from './component/Sample'
 import HomePage from './component/HomePage'
 import Contact from './component/Contact'
 import { Route, Routes } from 'react-router-dom'
+import Profile from './component/Profile'
+import { UserContext } from './UserContext'
+import Search from './component/Search'
+import Products from './component/Products'
 
 
 function App() {
   const [count, setCount] = useState(0)
+  const name="Profile Page Name"
   
 
   return (
     <>
+      <UserContext.Provider value={name}>
+        <Routes>
+          <Route path='homepage' element={<HomePage/>}>
+            <Route path='sample' element={<Sample/>}/>
+          </Route>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/profile/:username' element={<Profile/>}/>
+          <Route path="/search" element={<Search/>}/>
+          <Route path='/product' element={<Products/>}/>
+        </Routes>
+      </UserContext.Provider>
 
-      <Routes>
-        <Route path='/' element={<><HomePage/><Sample/></>}/>
-        <Route path='/contact' element={<Contact/>}/>
-      </Routes>
     </>
   )
 }
